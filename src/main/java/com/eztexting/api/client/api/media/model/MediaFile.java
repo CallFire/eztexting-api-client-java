dd2e0d081e7bba3779c29340a14bcfd47b09da07
ZelNaga <svirhyn@gmail.com>
2018-03-06 18:28:19 +0200

    automatic release to github and remove lombok

--- src/main/java/com/eztexting/api/client/api/media/model/MediaFile.java
+++ src/main/java/com/eztexting/api/client/api/media/model/MediaFile.java
@@ -1,14 +1,41 @@
 package com.eztexting.api.client.api.media.model;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 import com.eztexting.api.client.api.common.model.EzTextingModel;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Data;
-
-@Data
 public class MediaFile extends EzTextingModel {
     @JsonProperty("ID")
     private Long id;
     private String name;
     private String path;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .appendSuper(super.toString())
+                .append("id", id)
+                .append("name", name)
+                .append("path", path)
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
 }
